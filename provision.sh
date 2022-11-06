6c82ae9cd1553c25e2ddd7be9dfe5605a2508db3
Emon46 <emon@appscode.com>
2022-11-07 03:10:55 +0600

    Add Load-tester
    Signed-off-by: Emon46 <emon@appscode.com>

--- provision.sh
+++ provision.sh
@@ -13,7 +13,7 @@ helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
 helm upgrade -i metrics-server metrics-server/metrics-server --devel --set args[0]=--kubelet-insecure-tls --create-namespace  --namespace kube-metric
 
 sleep 30
-
+kubectl apply -f tel-agent/rbac.yaml
 kubectl apply -f tel-agent/service.yaml
 kubectl apply -f tel-agent/tel-agent-config.yaml
 kubectl apply -f tel-agent/vector-ta-sts.yaml
